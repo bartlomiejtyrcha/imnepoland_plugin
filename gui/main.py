014f639c31a221749dddb4fae4760b1f779502e3
dominik097 <cywil899@gmail.com>
2021-07-03 19:43:23 +0200

    main dialog

---
+++ gui/main.py
@@ -0,0 +1,13 @@
+import os
+
+from qgis.PyQt import uic
+from qgis.PyQt import QtWidgets
+
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'main.ui'))
+
+class MainDialog(QtWidgets.QDialog, FORM_CLASS):
+    def __init__(self, parent=None):
+        """Constructor."""
+        super(MainDialog, self).__init__(parent)
+        self.setupUi(self)
